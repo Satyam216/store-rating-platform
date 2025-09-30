34d2ae0659361b493f17f507efc3e57f20a97a1b
Satyam216 <satyamjain216@gmail.com>
2025-09-30 20:47:40 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
-    dialect: "mysql", 
+    dialect: process.env.DB_DIALECT,
     logging: false,
   }
 );
